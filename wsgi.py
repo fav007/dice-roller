025eead8d00cad3050c6b6f2f87c6ba2d7f6e0f1
fav007 <89335001+fav007@users.noreply.github.com>
2022-12-16 15:59:51 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,3 +4,5 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return jsonify({'roll':random.randint(1,6)})
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
